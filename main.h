e7ba4c3403ef6d8fc84ced3f1f143b3fc5b1a2da
imsrkulkarni <77853418+imsrkulkarni@users.noreply.github.com>
2022-06-28 12:01:10 +0530

    Update main.h

--- main.h
+++ main.h
@@ -1,6 +1,6 @@
 #include <stdio.h>
-#define MAX_TEMP 10;
-#define MAX_READING_LIMIT 4094;
+#define MAX_TEMP 10
+#define MAX_READING_LIMIT 4094
 int getNoOfRValuesInRange(int* array_p,int startRange ,int endRange);
 bool checkInRange(int val,int startRange ,int endRange);
 int  ampConverter(int reading);
